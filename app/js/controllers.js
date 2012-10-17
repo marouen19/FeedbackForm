d97286e37414fe616f832c675dca4b7c7405af0e
Marouen Zelleg <marouen19@gmail.com>
2012-10-18 05:31:57 +0800

    Form created
    missing last part (project info)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,14 +6,18 @@
 function MyCtrl1($scope) {
 	$scope.trainingId='Training';
 	$scope.trainings=[
-			{'id':'sgcl','name':'Intalio|Create Training - Singapore'},
-			{'id':'inbpms','name':'Intalio|BPMS Training - India'}
+			{'trainingId':'sgcl','name':'Intalio|Create Training - Singapore'},
+			{'trainingId':'inbpms','name':'Intalio|BPMS Training - India'}
 	];
+	$scope.trainingSelected={'trainingId':'initial','name':'Intalio Training'};
+	$scope.rating=[
+	{"order":0,"value":"Poor"},{"order":1,"value":"Average"},{"order":2,"value":"Good"},{"order":3,"value":"Excellent"}]
 }
 //MyCtrl1.$inject = ['$scope'];
 
 
 
-function MyCtrl2() {
+function MyCtrl2($scope, $routeParams) {
+	$scope.selectedTraining=$routeParams.selectedTraining;
 }
-MyCtrl2.$inject = [];
+//MyCtrl2.$inject = [];
