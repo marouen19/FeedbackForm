5c202e2670768c472809ee7fc47f01df437d2ff8
Marouen Zelleg <marouen19@gmail.com>
2012-10-17 23:53:56 +0800

    This is the starting point of the feedback form
    Feedback Form for training. Using AngularJS.
    Later would get the data via datamapper

---
+++ app/js/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Controllers */
+
+
+function MyCtrl1($scope) {
+	$scope.trainingId='Training';
+}
+//MyCtrl1.$inject = ['$scope'];
+
+
+
+function MyCtrl2() {
+}
+MyCtrl2.$inject = [];
