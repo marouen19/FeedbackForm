409c0372cc643b6150641fe53a5244a8af392ce3
Marouen Zelleg <marouen19@gmail.com>
2012-10-18 02:47:49 +0800

    Top Banner works

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,10 @@
 
 function MyCtrl1($scope) {
 	$scope.trainingId='Training';
+	$scope.trainings=[
+			{'id':'sgcl','name':'Intalio|Create Training - Singapore'},
+			{'id':'inbpms','name':'Intalio|BPMS Training - India'}
+	];
 }
 //MyCtrl1.$inject = ['$scope'];
 
